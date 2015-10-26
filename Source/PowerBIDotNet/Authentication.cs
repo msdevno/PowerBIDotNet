4e857dd540b4e37944fcbd089e29cae9358f3d54
Einar Ingebrigtsen <einar@dolittle.com>
2015-10-26 14:31:29 +0100

    Authentication was not there

---
+++ Source/PowerBIDotNet/Authentication.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace PowerBIDotNet
+{
+    public class Authentication : IAuthentication
+    {
+        // https://auth0.com/docs/connections/enterprise/azure-active-directory
+
+        const string AuthorityUri = "https://login.windows.net/common/oauth2/authorize";
+        const string RedirectUri = "https://login.live.com/oauth20_desktop.srf";
+        const string ResourceUri = "https://analysis.windows.net/powerbi/api";
+
+        public Tokens GetTokens(Client client)
+        {
+            var tokenCache = new TokenCache();
+            var authenticationContext = new AuthenticationContext(AuthorityUri, tokenCache);
+            var result = authenticationContext.AcquireToken(ResourceUri, client, new Uri(RedirectUri), PromptBehavior.RefreshSession);
+
+            var tokens = new Tokens
+            {
+                AccessToken = result.AccessToken,
+                RefreshToken = result.RefreshToken
+            };
+
+            return tokens;
+        }
+
+        public Tokens GetTokens(Client client, ClientSecret clientSecret, Token token, string redirectUri)
+        {
+            var tokenCache = new TokenCache();
+
+            var authenticationContext = new AuthenticationContext(AuthorityUri, tokenCache);
+            var clientCredential = new ClientCredential(client, clientSecret);
+
+            var result = authenticationContext.AcquireTokenByAuthorizationCode(token, new Uri(redirectUri), clientCredential);
+            var tokens = new Tokens
+            {
+                AccessToken = result.AccessToken,
+                RefreshToken = result.RefreshToken
+            };
+            return tokens;
+        }
+
+        public Tokens RefreshToken(Client client, Token refreshToken)
+        {
+            var tokenCache = new TokenCache();
+            var authenticationContext = new AuthenticationContext(AuthorityUri, tokenCache);
+            var result = authenticationContext.AcquireTokenByRefreshToken(refreshToken, client, ResourceUri);
+            var tokens = new Tokens
+            {
+                AccessToken = result.AccessToken,
+                RefreshToken = result.RefreshToken
+            };
+            return tokens;
+        }
+    }
+}
