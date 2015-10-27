4c7472aadeaf9f6d613d77874412d8c95f092777
Einar Ingebrigtsen <einar@dolittle.com>
2015-10-27 18:17:53 +0100

    Adding license header to all files

--- Source/PowerBIDotNet/Dashboard.cs
+++ Source/PowerBIDotNet/Dashboard.cs
@@ -1,4 +1,7 @@
-﻿namespace PowerBIDotNet
+﻿// Copyright (c) Microsoft. All rights reserved. 
+// Licensed under the MIT license. See LICENSE file in the project root for full license information. 
+
+namespace PowerBIDotNet
 {
     public class Dashboard
     {
--- Source/PowerBIDotNet/IWorkspace.cs
+++ Source/PowerBIDotNet/IWorkspace.cs
@@ -1,4 +1,7 @@
-﻿// https://msdn.microsoft.com/en-us/library/mt243840.aspx
+﻿// Copyright (c) Microsoft. All rights reserved. 
+// Licensed under the MIT license. See LICENSE file in the project root for full license information. 
+
+// https://msdn.microsoft.com/en-us/library/mt243840.aspx
 
 using System.Collections.Generic;
 
