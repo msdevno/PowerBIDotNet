c191333a7c16339808334fecff9e950ddc04ded4
Einar Ingebrigtsen <einar@dolittle.com>
2016-01-30 22:46:04 +0100

    Putting in missing information in XML comments

--- Source/PowerBIDotNet/Report.cs
+++ Source/PowerBIDotNet/Report.cs
@@ -4,7 +4,7 @@
 namespace PowerBIDotNet
 {
     /// <summary>
-    /// Represents a report in a <see cref="Import">import</see>
+    /// Represents a report in a <see cref="Report">report</see>
     /// </summary>
     public class Report
     {
@@ -19,12 +19,12 @@ namespace PowerBIDotNet
         public string Name { get; set; }
 
         /// <summary>
-        /// 
+        /// Gets or sets the URL that can be used to navigate directly to a <see cref="Report"/>
         /// </summary>
         public string WebUrl { get; set; }
 
         /// <summary>
-        /// 
+        /// Gets or sets the URL that can be used for embedding the <see cref="Report"/> into your own application
         /// </summary>
         public string EmbedUrl { get; set; }
     }
