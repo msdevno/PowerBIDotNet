7de58bfe369029b4bac90c420c12c5e09bbedc4f
Einar Ingebrigtsen <einar@dolittle.com>
2016-01-30 22:40:23 +0100

    Moving the DataSetCreate command into Commands namespace - cleanup

--- Source/PowerBIDotNet/Commands/DatasetCreate.cs
+++ Source/PowerBIDotNet/Commands/DatasetCreate.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved. 
 // Licensed under the MIT license. See LICENSE file in the project root for full license information. 
 
-namespace PowerBIDotNet
+namespace PowerBIDotNet.Commands
 {
     /// <summary>
     /// Represents the operation of creating a <see cref="Dataset"/>
--- Source/PowerBIDotNet/Datasets.cs
+++ Source/PowerBIDotNet/Datasets.cs
@@ -5,6 +5,7 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using PowerBIDotNet.Wrappers;
+using PowerBIDotNet.Commands;
 
 namespace PowerBIDotNet
 {
